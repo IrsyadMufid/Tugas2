dad39c370de4fff83945784f613ea9e66c6ac6c6
irsyadmufid <irsyadm90@gmail.com>
2022-09-13 15:11:41 +0700

    Perbaikan nama

--- katalog/views.py
+++ katalog/views.py
@@ -5,7 +5,7 @@ def show_katalog(request):
     data_barang_katalog = CatalogItem.objects.all()
     context = {
     'list_barang': data_barang_katalog,
-    'nama': 'Muhammad irsyad Mufid',
+    'nama': 'Muhammad Irsyad Mufid',
     'NPM' : '2106752073'
 }
     return render(request, "katalog.html", context)
