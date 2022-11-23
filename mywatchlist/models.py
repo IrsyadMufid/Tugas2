aad8d53e2a4ae69c337b0ed88752e34627982a3c
irsyadmufid <irsyadm90@gmail.com>
2022-11-24 00:25:54 +0700

    long

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
+from datetime import datetime
 
 
 class MyWatchlist(models.Model):
